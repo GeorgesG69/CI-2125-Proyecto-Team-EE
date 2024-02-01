252317afe4110cc1f7c43ea0a251af72e136e125
GeorgesG69 <cddde123@gmail.com>
2024-02-01 11:27:53 -0400

    Comentarios del código

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from colorama import Back, Fore, init
 init()
 
 def main(page: ft.Page):
-    page.title = "Calculadora de Resistores"
-    #page.bgcolor = "#1a0f2a"
+    page.title = "Calculadora de Resistores" # Título del borde de la ventana
+    #page.bgcolor = "#1a0f2a" # Color del fondo
     def Titulo():
 
-        Title = ft.Text(value="Calculadora de Resistores", size=30)
+        Title = ft.Text(value="Calculadora de Resistores", size=30) # Título del centro de la apliacción
 
         return ft.Row(controls=[Column(controls=[
 
             Title,
         ])], alignment=MainAxisAlignment.CENTER)
     
-    SelColores = ft.Row(alignment=MainAxisAlignment.CENTER)
-    Colores = ft.Row(expand=False, scale=1.25)
+    SelColores = ft.Row(alignment=MainAxisAlignment.CENTER) # Fila de selectores de colores
+    Colores = ft.Row(expand=False, scale=1.25) # Bandas del resistor, cambian de color
+
+    # Colores con sus códigos
 
     Color_0 = "#000000"  # Negro
     Color_1 = "#883c00"  # Marrón
@@ -39,9 +41,18 @@ def main(page: ft.Page):
     B_5 = ft.Container(bgcolor="#ffffff", width=15, height=170, border=ft.border.all(2, "#000000"), )
     B_6 = ft.Container(bgcolor="#ffffff", width=15, height=170, border=ft.border.all(2, "#000000"), )
 
-
+    # Función para cambiar la cantidad de bandas
     def Resistor(e):
-        if e.control.value == "1":
+
+        # Los valores que aparecen en cada condicional, es decir "e.control.value" son introducidos
+        # por los selectores en forma de punto. Los que se guardan en la variable "Selector".
+        # "e" es la letra que representa el "evento" en la aplicación, específicamente en esta función
+        # se trabaja con los eventos que envian los selectores de punto.
+
+        # Cada punto agrega la cantidad de bandas sobre el dibujo del resistor. 
+        # Es lo que hace cada condicional a continuación:
+
+        if e.control.value == "1": 
 
             SelColores.controls = [S1, S2, ft.VerticalDivider(width=50), SMultiplicador_3]
             
@@ -109,7 +120,8 @@ def main(page: ft.Page):
 
             page.update()
     
-    # Selectores
+    # Selectores del color para cada banda
+    # Nota: Hay que buscar una forma de colocar colores en cada lista. Por ahora tienen emojis.
 
     S1 = ft.Dropdown(options=[
 
@@ -220,23 +232,23 @@ def main(page: ft.Page):
 
             ], width=60, label="TCR", bgcolor="#ffffff")
      
-    IMG_R = ft.Image(src="Resistor 2.png", scale=1.25)
-    Imagen = ft.Row(controls=[ft.Stack(controls=[
-        
+    IMG_R = ft.Image(src="Resistor 2.png", scale=1.25) # Dibujo del resistor
+
+    Imagen = ft.Row(controls=[ft.Stack(controls=[      # Fila que contiene un "Stack", un elemento que sirve para poner cosas
+                                                       # encima de otras
         IMG_R,
-        
         Colores,
-        
         
     ],)], alignment=MainAxisAlignment.CENTER)
             
-            
-        
+    # Etiquetas de cada selector de punto. Tambien se pueden colocar en la propia función "ft.Radio". Pero preferí ponerla así.
 
     TBandas = "3 Bandas"
     CuBandas = "4 Bandas"
     CiBandas = "5 Bandas"
     SBandas = "6 Bandas"
+
+    # Fila con los selectores de punto
 
     Selector = ft.Row(controls=[ft.RadioGroup(content=ft.Row(controls=[
 
@@ -256,7 +268,8 @@ def main(page: ft.Page):
     , on_change=Resistor)], alignment=MainAxisAlignment.CENTER)
     
     
-    
+    # Acá se añaden los elementos a la interfaz. 
+
     page.add(Titulo(), SelColores)
     page.add(ft.Divider(opacity=0, height=25))
     page.add(Imagen)
@@ -265,4 +278,4 @@ def main(page: ft.Page):
     
     page.add(Colores)
 
-ft.app(target=main)
+ft.app(target=main) # Arranque de la aplicación
